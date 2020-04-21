89162ac443590527ac755019ae7e113316647ad7
Andrew Lim <2000andrewlim@gmail.com>
2020-04-20 22:47:16 -0700

    update dburl

--- app.js
+++ app.js
@@ -17,14 +17,15 @@ var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb+srv://dbUser:tvBoL1zNYQFbeELW@cluster0-30abl.mongodb.net/test?retryWrites=true&w=majority", {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("Connected to DB!");
-}).catch(err => {
-  console.log("ERROR:", err.message);
-});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb+srv://dbUser:tvBoL1zNYQFbeELW@cluster0-30abl.mongodb.net/test?retryWrites=true&w=majority", {
+//   useNewUrlParser: true, 
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log("Connected to DB!");
+// }).catch(err => {
+//   console.log("ERROR:", err.message);
+// });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
